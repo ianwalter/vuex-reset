ef1c57e9030610b4c2165d9f07a68ef4f58f8516
Ian Walter <public@iankwalter.com>
2018-10-16 23:32:48 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import clone from '@ianwalter/clone'
+
+export default (opts = {}) => {
+  const { ssr, trigger } = Object.assign({}, { trigger: 'reset' }, opts)
+
+  return store => {
+    // Extract the initial state from the store so that it can be used to reset
+    // the store when a trigger mutation is executed.
+    const initialState = clone(store.state, { objectCreate: false })
+
+    // If the Vuex store needs to be hydrated from SSR data, add it to the store
+    // after the initialState is set so that initialState isn't polluted with
+    // SSR data and the store can be reset cleanly.
+    if (ssr) {
+      // console.log({ ...initialState, ...ssr })
+      // store.replaceState({ ...initialState, ...ssr })
+      store.replaceState(Object.assign({}, initialState, ssr))
+    }
+
+    store.subscribe((mutation, state) => {
+      if (mutation.type === trigger) {
+        // TODO: don't reset route module if set.
+
+        // Reset the entire store state.
+        store.replaceState(clone(initialState))
+      } else {
+        // Extract the name of the module and mutation.
+        const [mod, mut] = mutation.type.split('/')
+
+        if (mut === trigger) {
+          // Reset the state for the module containing the mutation.
+          const moduleState = { [mod]: initialState[mod] }
+          const newState = Object.assign({}, state, moduleState)
+          store.replaceState(clone(newState, { objectCreate: false }))
+        }
+      }
+    })
+  }
+}
---
+++ tests/plugin.test.js
@@ -0,0 +1,71 @@
+const Vue = require('vue')
+const Vuex = require('vuex')
+const clone = require('@ianwalter/clone')
+
+const VuexReset = require('../')
+
+Vue.use(Vuex)
+
+test('plugin resets state when trigger mutation executed', () => {
+  const state = { message: 'Hello!' }
+  const store = new Vuex.Store({
+    plugins: [VuexReset()],
+    state: clone(state),
+    mutations: {
+      message: (state, message) => (state.message = message),
+      reset: () => {}
+    }
+  })
+  store.commit('message', 'Greetings!')
+  store.commit('reset')
+  expect(store.state).toEqual(state)
+  store.commit('message', 'Yo!')
+  store.commit('reset')
+  expect(store.state).toEqual(state)
+})
+
+test('plugin resets only module state when module mutation executed', () => {
+  const rootMessage = 'Yo!'
+  const songName = 'One Touch'
+  const state = { message: 'Hello!' }
+  const songState = { name: 'Messy Love' }
+  const store = new Vuex.Store({
+    plugins: [VuexReset()],
+    state: clone(state),
+    mutations: {
+      message: (state, message) => (state.message = message),
+      reset: () => {}
+    },
+    modules: {
+      song: {
+        namespaced: true,
+        state: clone(songState),
+        mutations: {
+          name: (state, name) => (state.name = name),
+          reset: () => {}
+        }
+      }
+    }
+  })
+  store.commit('message', rootMessage)
+  store.commit('song/name', songName)
+  expect(store.state.song.name).toEqual(songName)
+  store.commit('song/reset')
+  expect(store.state.message).toEqual(rootMessage)
+  expect(store.state.song).toEqual(songState)
+})
+
+test('plugin uses ssr state but can reset to initial state', () => {
+  const message = 'Yo!'
+  const state = { message: 'Hello!' }
+  const store = new Vuex.Store({
+    plugins: [VuexReset({ ssr: { message } })],
+    state: clone(state),
+    mutations: {
+      reset: () => true
+    }
+  })
+  expect(store.state.message).toBe(message)
+  store.commit('reset')
+  expect(store.state).toEqual(state)
+})
