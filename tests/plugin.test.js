b8bb4e46cf463d81b9307af60577e31ef8d1c76a
Ian Walter <public@iankwalter.com>
2019-01-05 22:12:24 -0500

    Trying to get GA workflow to pass

--- tests/plugin.test.js
+++ tests/plugin.test.js
@@ -2,7 +2,7 @@ const Vue = require('vue')
 const Vuex = require('vuex')
 const clone = require('@ianwalter/clone')
 
-const VuexReset = require('../')
+const VuexReset = require('../dist/vuex-reset.js')
 
 Vue.use(Vuex)
 
